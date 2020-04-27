89487f3cdff26fa014be4a95dda0343f89e75600
rymksontini <rymksontini@gmail.com>
2020-04-28 00:31:56 +0100

    first commit

---
+++ proxy-service/src/main/java/tn/insat/tpmicro/proxyservice/ProxyServiceApplication.java
@@ -0,0 +1,13 @@
+package tn.insat.tpmicro.proxyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyServiceApplication.class, args);
+	}
+
+}
